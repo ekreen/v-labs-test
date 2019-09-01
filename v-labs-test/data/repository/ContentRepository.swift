465d72bbbdb4fb2047892da8b2de089b43db8f7d
Kévin Esprit <kevinesprit@gmail.com>
2019-09-01 22:10:02 +0200

    [FEAT] Add ContentRepository

---
+++ v-labs-test/data/repository/ContentRepository.swift
@@ -0,0 +1,29 @@
+//
+//  ContentRepository.swift
+//  v-labs-test
+//
+//  Created by Kévin Esprit on 01/09/2019.
+//  Copyright © 2019 Kespri. All rights reserved.
+//
+
+import RxSwift
+
+class ContentRepository {
+    private let apiManager: ApiManager
+    
+    init(apiManager: ApiManager) {
+        self.apiManager = apiManager
+    }
+    
+    func getUsers() -> Single<[User]> {
+        return apiManager.getUsers()
+    }
+    
+    func getAlbums(for userId: Int) -> Single<[Album]> {
+        return apiManager.getAlbums(for: userId)
+    }
+    
+    func getPosts(for userId: Int) -> Single<[Post]> {
+        return apiManager.getPosts(for: userId)
+    }
+}
